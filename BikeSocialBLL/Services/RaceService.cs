95035348741c821f771e55e8fc0f9c6ef71fc28d
Filipe <filipegajo95@gmail.com>
2022-03-25 15:10:28 +0000

    Criar prova (a funcionar)

--- BikeSocialBLL/Services/RaceService.cs
+++ BikeSocialBLL/Services/RaceService.cs
@@ -17,13 +17,11 @@ namespace BikeSocialBLL.Services
         
         public async Task<bool> Create(CreateRaceDto race)
         {
-            // throw new NotImplementedException();
-
             Race rc = await _raceRepository.Get(raceQuery => raceQuery.Description == race.description.ToString());
-
-            // verifica se existe alguma race com os dados (Description) que recebe de cima
-            if (rc == null || (await _raceRepository.Add(race.AsRace()) != null)) return false;
-            else return true;
+            
+            if (rc != null) return false;
+            else await _raceRepository.Add(race.AsRace());
+            return true;
         }
     }
 }
