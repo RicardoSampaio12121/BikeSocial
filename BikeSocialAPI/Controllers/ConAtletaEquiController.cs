f46bdf1ca919294972e869d0b1518cb0f1b69480
pcpr1994 <75262703+pcpr1994@users.noreply.github.com>
2022-03-28 14:54:17 +0100

    convite At Eq

---
+++ BikeSocialAPI/Controllers/ConAtletaEquiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BikeSocialDTOs;
+using BikeSocialBLL.Services.IServices;
+
+namespace BikeSocialAPI.Controllers
+{
+    [ApiController]
+    [Route("Convite de Atleta para Equipa")]
+    public class ConAtletaEquiController : Controller
+    {
+        private readonly IConAtletaEquiService _conAtletaEquiService;
+        private bool res;
+
+        public ConAtletaEquiController(IConAtletaEquiService conAtletaEquiService)
+        {
+            _conAtletaEquiService = conAtletaEquiService;
+        }
+
+        [HttpPost("convite")]
+        public async Task<IActionResult> Convite(CreateConvAtletaEquiDto convite)
+        {
+            if (await _conAtletaEquiService.ConviteAE(convite, res) == false)
+                return BadRequest();
+       
+            return Ok();
+
+        }
+
+    }
+}
