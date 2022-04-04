e4f16cf1af94c2fd04c7d81372cba0c6ae6c907a
Filipe <filipegajo95@gmail.com>
2022-04-04 15:23:07 +0100

    Entities conquistas e tipos

---
+++ BikeSocialEntities/Achievements.cs
@@ -0,0 +1,12 @@
+﻿namespace BikeSocialEntities
+{
+    public class Achievements
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AchievementTypeId { get; set; }
+        public float achievementTime { get; set; }
+        public DateTime dateTime { get; set; }
+        public int PlacesId { get; set; }
+    }
+}
